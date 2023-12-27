eb2568d07a766c6bd054317bdb172546293ab44c
tiwaripalak245 <tiwaripalak836@gmail.com>
2023-12-27 14:24:22 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const display = document.querySelector(".display")
+const buttons = document.querySelectorAll("button")
+const specialchar = ["*", "/", "+", "-", "*", "="]
+let output = "";
+
+const calculate = (btnvalue) => {
+    if (btnvalue === "=" && output!== "") {
+     output = eval(output.replace("%", "/100"))   
+    }else if(btnvalue === "AC") {
+output = "";
+    } else if (btnvalue === "DEL") {
+        output = output.toString().slice(0, -1); 
+    } else {
+        if (output == "" && specialchar.includes(btnvalue)) return;
+        output += btnvalue
+    }
+display.value = output
+}
+
+buttons.forEach((button) => {
+button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+})
+;
+
+
+
+
